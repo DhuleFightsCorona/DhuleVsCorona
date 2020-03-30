2a1db71fb80abbb8acb50f15cc5c0aecd99187c2
pushpak1300 <pushpak1300@gmail.com>
2020-03-30 08:33:13 +0530

    Update and fix links

--- resources/views/livewire/bored.blade.php
+++ resources/views/livewire/bored.blade.php
@@ -2,7 +2,7 @@
 
   <div class="card-body">
     <h5 class="card-title">Bored In Qurantine</h5>
-    <p class="card-text">Let's find you something to do</p>
+    <p class="card-text">Why not make the quarntine more fun!?</p>
     <a wire:click='generate' wire:loading.class="disabled" class="btn btn-primary" style="color:white;">Generate Activity</a>
     @if($bored)
     <p class='mt-2'><mark><strong>Activity : {{$bored['activity']}}</strong></mark></p>
